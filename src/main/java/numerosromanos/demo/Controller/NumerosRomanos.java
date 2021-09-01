95ac79b50964ca0aa3410fd69702c8d052205131
allozano33 <alessandro.lozano@mercadolivre.com>
2021-09-01 17:09:45 -0300

    Initial commit

---
+++ src/main/java/numerosromanos/demo/Controller/NumerosRomanos.java
@@ -0,0 +1,39 @@
+package numerosromanos.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NumerosRomanos {
+
+
+    @GetMapping("/converter")
+    public numerosRomanos(@RequestParam Integer numero){
+        int[] valorDecimal = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        String[] valorRomano = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String res = null;
+        int i=0;
+        while(numero >=0 ) {
+            if (numero >= valorDecimal[i]) {
+                res = valorRomano[i];
+                numero -= valorDecimal(i);
+                else{
+                    i++;
+                }
+            }
+
+            return res;
+        }
+
+
+
+
+
+
+
+
+
+
